d4736a29553b04fdbf760e8fa4092a1cd8fa58f1
Npadilla05 <123766215+Npadilla05@users.noreply.github.com>
2023-04-04 16:29:55 -0500

    Add files via upload

---
+++ week_5/titanic.sql
@@ -0,0 +1,14 @@
+# Count number of rows in data        
+# SELECT
+# COUNT(*)
+# FROM titanic
+
+# Total of people who survived
+# SELECT SUM("Survived")
+# FROM titanic
+
+# Passenger class with largest population
+# SELECT COUNT("Pclass") as passenger_class_count
+# FROM titanic
+# GROUP BY "Pclass"
+# ORDER BY "Pclass" asc  -> 3rd class has largest with 487
